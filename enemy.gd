929ec3c5a71c188e24f847d3b0a03205eb886901
Fabián E. García <garciafabianenrique79@gmail.com>
2024-09-12 16:03:44 -0300

    animation Idle en la roca y comienzo de explotion

--- enemy.gd
+++ enemy.gd
@@ -1,6 +1,6 @@
 extends RigidBody2D
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	if area.is_in_group("laser") or area.is_in_group("nave"):
+	if area.is_in_group("laser") or area.is_in_group("nave"): #revisar el or que no anda
 		queue_free()	
 	pass 
