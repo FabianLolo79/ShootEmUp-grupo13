9871a3b37509b43fc9105abe00a56edb96c71c7d
Fabián E. García <garciafabianenrique79@gmail.com>
2024-09-12 13:25:28 -0300

    feature laser, fondo, música

--- nave.gd
+++ nave.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 # variable para la instancia del disparo
-var shoot = true
+var shooting = true
 var pre_laser = preload("res://Scenes/laser.tscn")
 
 #velocidad de movimiento player
@@ -17,18 +17,18 @@ func Move():
 	if direction:
 		velocity.x = direction * SPEED
 	else: 
-		velocity.x = 0
+		velocity.y = 0
 	move_and_slide()	
 	pass
 	
 func Shoot():
-	if Input.is_action_just_pressed("ui_up") and shoot:
+	if Input.is_action_just_pressed("ui_up") and shooting:
 		var laser = pre_laser.instantiate()
 		get_parent().add_child(laser)
 		laser.position.x = position.x
 		laser.position.y = position.y -50
-		shoot = false
+		shooting = false
 		await get_tree().create_timer(0.5).timeout
-		shoot = true
+		shooting = true
 		pass
 		
